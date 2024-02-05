644e9d51b6796bae27459d351ffaed57ca307066
BehnooshNasri <behnoosh.nasri@gmail.com>
2024-02-04 22:03:53 -0500

    Create ERD, SQL database, schema and tables

---
+++ Crowdfunding_ETL_tables_verification.sql
@@ -0,0 +1,18 @@
+--- Import the tables based on their constraints. campaign.csv should be the last one to import due to the existence multiple foreign key constraints. 
+--- To test each table highlight each one to run. 
+
+
+SELECT * 
+FROM contacts; 
+
+
+SELECT * 
+FROM category; 
+
+
+SELECT * 
+FROM subcategory;
+
+
+SELECT * 
+FROM campaign;
